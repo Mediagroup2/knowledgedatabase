31d3f4f40c2125d0bfcf4a299b6c3ca0dc707a79
Remmert <(D179676.edu@rocwb.nl)>
2014-03-14 15:35:59 +0100

    Bijna klaar g

---
+++ add_psd.php
@@ -0,0 +1,28 @@
+<?php
+include 'connect.inc.php';
+error_reporting(E_ALL ^ E_NOTICE);
+ mysql_connect('localhost', 'root', '')or die ("can not connect");
+ mysql_select_db('media_group2') or die ("can not connect");
+   
+ $titel = mysql_real_escape_string($_POST['titel']);
+ $tags = mysql_real_escape_string($_POST['tags']);
+ $categorie = mysql_real_escape_string($_POST['categorie']);
+ $url = mysql_real_escape_string($_POST['url']);
+ $beschrijving = mysql_real_escape_string($_POST['beschrijving']);
+ $toegevoegd_op = mysql_real_escape_string($_POST['toegevoegd_op']);
+  $queryEdit = mysql_query("UPDATE psd SET titel = '$titel', tags = '$tags', categorie = '$categorie', url = '$url', beschrijving = '$beschrijving', toegevoegd_op = '$toegevoegd_op', WHERE id= '$id'");
+  if ($queryEdit) {
+           header('location: toevoegen_psd.php');
+
+  }
+if (!empty($titel) OR ($url)) {
+ $query = mysqli_query($con, "INSERT INTO psd (titel, tags, categorie, url, beschrijving, toegevoegd_op) VALUES ('$_POST[titel]','$_POST[tags]','$_POST[categorie]','$_POST[url]','$_POST[beschrijving]','$_POST[toegevoegd_op]')");
+  mysql_close();
+ echo"Video succesvol toegevoegd";
+ } else{
+ echo "Je hebt niet alle velden ingevuld";
+ }
+ ?>
+<html>
+  <a href="toevoegen_psd.php"> Terug naar videos</a>
+</html>
---
+++ toevoegen_jquery.php
@@ -0,0 +1,38 @@
+<?php
+include 'connect.inc.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>toevoegen_jquery</title>
+</head>
+<body>
+    <div class="container">
+    <form action="add_jquery.php" method="post">
+     <input type="text" name="titel" placeholder="insert titel"><br>
+           <input type="text" name="tags" placeholder="insert tags"><br>
+<input type="radio" name="verzekering" value="html">html
+<input type="radio" name="verzekering" value="css">css 
+<input type="radio" name="verzekering" value="php">php
+<input type="radio" name="verzekering" value="jquery">jquery
+<input type="radio" name="verzekering" value="psd">psd
+                           <input type="text" name="url" placeholder="url"><br>
+                        <input type="text" name="beschrijving" placeholder="insert beschrijving"><br>
+                       <input type="date" name="datum" placeholder="datum"><br>
+                     <input type="submit" value="Toevoegen">
+                     <input type="submit" value="Annuleren">
+                     </form>
+               </tr>
+                 </div>
+                 </html>
+                 <?php
+                $querySelect = mysql_query("SELECT * FROM jquery");
+$queryFetch = ($querySelect);
+ echo "<tr><td>".$queryFetch['titel']."</td>";
+ echo "<td>".$queryFetch['tags']."</td>";
+    echo "<td>".$queryFetch['categorie']."</td>";
+    echo "<td>".$queryFetch['url']."</td>";
+    echo "<td>".$queryFetch['beschrijving']."</td>";
+    echo "<td>".$queryFetch['toegevoegd_op']."</td>";
+                
+                 ?>
